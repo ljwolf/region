94ee4e5c66fd214ecb6a27e022ff03f9a29dcb91
ljwolf <levi.john.wolf@gmail.com>
2018-05-11 17:44:53 +0100

    try removing install-requires

--- setup.py
+++ setup.py
@@ -76,5 +76,5 @@ if __name__ == "__main__":
             'pysal'
         ],
 
-        python_requires='~=3.3',  # ~=3.3 means >=3.3 and <4
+#        python_requires='~=3.3',  # ~=3.3 means >=3.3 and <4
     )
